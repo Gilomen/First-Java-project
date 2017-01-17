a1d87c920803153fb20580b560f34ce8c60be729
Gilomen <bettina.gilomen@gmail.com>
2017-01-17 21:48:13 +0100

    Update FontInterface.java

--- src/ascii/FontInterface.java
+++ src/ascii/FontInterface.java
@@ -4,7 +4,7 @@ package ascii;
 
 //import font.FontInterface; not possible if same name as in existing package:
 
-public interface FontInterface extends font.FontInterface //extends links FontInterface in font & ascii
+public interface FontInterface extends font.FontInterface //"extends" links FontInterface in font & ascii
 
 {
 	public char getSymbolDefault();
